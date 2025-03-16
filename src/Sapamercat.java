6431cf49ec71e1d22fef7c7e61f8291ab46665f0
Omar Ouahoud Kaaouachi <ok-omar@users.noreply.github.com>
2025-03-16 18:30:49 +0100

    Implemented the personalised exceptions and handling them

--- src/Sapamercat.java
+++ src/Sapamercat.java
@@ -96,7 +96,6 @@ public class Sapamercat {
      */
     public static void getProductes(){
         while (true){
-
             // Mostrar el menu Producte
             View.displayProducteMenu();
 
@@ -107,22 +106,33 @@ public class Sapamercat {
             scanner.nextLine();
 
             // Switch case per cridar la funcio corresponent al tipus de producte que l'usuari vol introduir
-            switch (input){
-                case 1:
-                    // Afegir un producte tipus Alimentacio al carret
-                    addIfApplicable(getAlimentacio());
-                    break;
-                case 2:
-                    // Agafar un producte tipus Textil
-                    addIfApplicable(getTextil());
-                    break;
-                case 3:
-                    // Afegir un producte tipus Electronica al carret
-                    addIfApplicable(getElectronica());
-                    break;
-                case 0:
-                    return;
+            try {
+                switch (input){
+                    case 1:
+                        // Afegir un producte tipus Alimentacio al carret si no hem arribat al limit (100)
+                        if (!Model.cartLimitReached()) Model.addToCart(getAlimentacio());
+                        break;
+                    case 2:
+                        // Agafar un producte tipus Textil si no hem arribat al limit (100) y no hi ha duplicats
+                        addIfNotDuplicate(getTextil());
+                        break;
+                    case 3:
+                        // Afegir un producte tipus Electronica al carret si no hem arribat al limit (100)
+                        if (!Model.cartLimitReached()) Model.addToCart(getElectronica());
+                        break;
+                    case 0:
+                        return;
+                }
+            } catch (LimitProductesException e){
+                System.out.println(e);
+                System.out.println("El producte no s'afegira al carret!");
+            } catch (DataCaducidadException e){
+                System.out.println(e);
+                System.out.println("Si us plau, introdueix una data de caducitat valida amb el format dd/mm/yyyy");
+            } catch (NegatiuException e) {
+                System.out.println(e);
             }
+
         }
 
     }
@@ -132,7 +142,7 @@ public class Sapamercat {
      * Funcio per agafar un producte (Alimentacio) introduit per l'usuari
      * @return L'objecte (Alimentacio)
      */
-    public static Producte getAlimentacio(){
+    public static Producte getAlimentacio() throws DataCaducidadException, NegatiuException {
         System.out.println("Afegir aliment");
         System.out.print("Nom producte: ");
         String nom = scanner.nextLine();
@@ -144,6 +154,12 @@ public class Sapamercat {
         System.out.print("Data de caducitat (dd/mm/yyyy): ");
         String dataCaducidad = scanner.nextLine();
 
+        boolean matches = dataCaducidad.matches("^\\d{2}\\/\\d{2}\\/\\d{4}");
+
+        if (preu < 0) throw new NegatiuException("El preu no pot ser negatiu", preu);
+
+        if (!matches) throw new DataCaducidadException("La data de caducitat té el format incorrecte!");
+
         return new Alimentacio(preu, nom, barcode, dataCaducidad);
     }
 
@@ -151,7 +167,7 @@ public class Sapamercat {
      * Funcio per agafar un producte (Textil) introduit per l'usuari
      * @return L'objecte (Textil)
      */
-    public static Producte getTextil(){
+    public static Producte getTextil() throws NegatiuException {
         System.out.println("Afegir Textil");
         System.out.print("Nom producte: ");
         String nom = scanner.nextLine();
@@ -163,7 +179,7 @@ public class Sapamercat {
         System.out.print("Codi de barres: ");
         String barcode = scanner.nextLine();
 
-
+        if (preu < 0) throw new NegatiuException("El preu no pot ser negatiu", preu);
         return new Textil(preu, nom, barcode, composicio);
     }
 
@@ -171,7 +187,7 @@ public class Sapamercat {
      * Funcio per agafar un producte (Electronica) introduit per l'usuari
      * @return L'objecte (Electronica)
      */
-    public static Producte getElectronica(){
+    public static Producte getElectronica() throws NegatiuException {
         System.out.println("Afegir Electronica");
         System.out.print("Nom producte: ");
         String nom = scanner.nextLine();
@@ -184,7 +200,7 @@ public class Sapamercat {
         System.out.print("Codi de barres: ");
         String barcode = scanner.nextLine();
 
-
+        if (preu < 0) throw new NegatiuException("El preu no pot ser negatiu", preu);
         return new Electronica(preu, nom, barcode, garantia);
     }
 
@@ -196,23 +212,29 @@ public class Sapamercat {
                 // Verificar si el numbero en el rang 0-4
                 if (number >= 0 && number <= 4) {
                     return number;
+                } else if (number < 0) {
+                    throw new NegatiuException("El numero introduit no pot negatiu", number);
                 } else {
-                    System.out.println("Numero invalid, introduiex un numero en el rang (0-4)");
+                    System.out.print("Numero invalid, introduiex un numero en el rang (0-4): ");
                 }
 
             } catch (InputMismatchException e) {
-                System.out.println("El valor ha de ser un numbero");
+                System.out.println("El valor ha de ser un numero: ");
                 scanner.next(); // Elimininar invalid input
+            } catch (NegatiuException e){
+                System.out.println(e);
             }
         }
     }
 
-    private static void addIfApplicable(Producte t){
+    private static void addIfNotDuplicate(Producte t) throws LimitProductesException {
         // Verificar si el producte introduit ja existeix al carret
         boolean duplicate = Model.verifyDuplicates(t);
 
         // Si no existeix, afegir el poducte al carret,
-        if (!duplicate) Model.addToCart(t);
+        if (!duplicate) {
+            Model.addToCart(t);
+        }
             // Si es un que ja existeix (duplicat), avisar l'usuari
         else System.out.printf("%s amb el codi de barres %s ja existeix! %n", t.getNom(), t.getBarcode());
     }
